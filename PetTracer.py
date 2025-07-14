c176e6cfb5ee90ea42a7c67c7d99820c77d5f13f
Yun Zhao <yunzhao7767@gmail.com>
2025-07-14 15:35:28 +1000

    Update

--- PetTracer.py
+++ PetTracer.py
@@ -89,14 +89,13 @@ def plot_posterior(title, parameter_name, cm, abc=None, xl = None, xr = None):
         sns.kdeplot(abc, color=custom_colors[1], linewidth=2, label='ABC')
     sns.kdeplot(cm, color=custom_colors[6],linewidth=2, label='CM')
 
-    plt.xlabel(parameter_name,fontsize=14,fontname="Arial", fontweight='bold', labelpad=10)
-    plt.ylabel('Density', fontsize=14,fontname="Arial", fontweight='bold', labelpad=10)
+    plt.xlabel(parameter_name,fontsize=14, fontweight='bold', labelpad=10)
+    plt.ylabel('Density', fontsize=14, fontweight='bold', labelpad=10)
     plt.tick_params(axis='both', labelsize=10)
     plt.legend(fontsize=10, loc='best')
     plt.title(title, fontsize=14)
     if xl is not None: 
         plt.xlim([xl,xr])
-    mpl.rcParams["font.family"] = "Arial"
     plt.show()
 
 def plot_TAC(data_path):
@@ -106,12 +105,12 @@ def plot_TAC(data_path):
                       105.0, 120.0, 180.0, 240.0, 300.0, 360.0, 420.0,
                       480.0, 600.0, 720.0, 840.0, 960.0, 1080.0, 1260.0,
                       1440.0, 1620.0, 1800.0, 2100.0, 2400.0, 2700.0, 3000.0])/60
-    fig = plt.figure(figsize=(10,7))
+    fig = plt.figure(figsize=(5,3.5))
     plt.plot(tspan, df.iloc[:,2],'--', label='Arterial input function', color='grey')
     plt.plot(tspan, df.iloc[:,3], 'k',label='Time-activity curve')
 
     plt.xlabel('Time (min)',fontweight='bold')
     plt.ylabel('Concentration (Bq/ml)',fontweight='bold')
-    plt.rcParams['font.size'] = 20
+    plt.rcParams['font.size'] = 14
     plt.legend(fontsize=18)
 
